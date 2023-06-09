eb1c0a45bfecd2c2a9b445c2ddd4dbdefd065ba5
rydwan92 <47033042+rydwan92@users.noreply.github.com>
2023-06-10 00:10:37 +0200

    Init

--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('blog', { title: 'Blog' });
 });
 
 module.exports = router;
--- routes/news.js
+++ routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('news', { title: 'News' });
 });
 
 module.exports = router;
